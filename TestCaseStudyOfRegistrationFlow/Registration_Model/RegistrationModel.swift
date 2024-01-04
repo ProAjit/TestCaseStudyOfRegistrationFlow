a6e72df3e18b01da7fad2e2d019ed8e3984d4473
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-01-04 11:00:06 +0530

    App registration model test case coverage 100%
    Get use to with test cases: Initial commit

---
+++ TestCaseStudyOfRegistrationFlow/Registration_Model/RegistrationModel.swift
@@ -0,0 +1,60 @@
+//
+//  RegistrationModel.swift
+//  TestCaseStudyOfRegistrationFlow
+//
+//  Created by Ajit Satarkar on 04/01/24.
+//
+
+import Foundation
+
+import Foundation
+
+protocol UserRegistrationModelValidatorProtocol { 
+    func isValidFirstName() -> Bool
+    func isValidLastName() -> Bool
+    func isValidEmail() -> Bool
+    func isValidPasswordLength() -> Bool
+    func doPasswordsMatch() -> Bool
+    func isValidPassword() -> Bool
+    func isDataValid() -> Bool
+}
+
+struct UserRegistrationModel {
+    let firstName: String
+    let lastName: String
+    let email: String
+    let password: String
+    let repeatPassword: String
+}
+
+extension UserRegistrationModel : UserRegistrationModelValidatorProtocol {
+    
+    func isValidFirstName() -> Bool {
+        return firstName.count > 1
+    }
+    
+    func isValidLastName() -> Bool {
+        return lastName.count > 1
+    }
+
+    func isValidEmail() -> Bool {
+        return email.contains("@") && email.contains(".")
+    }
+
+    func isValidPasswordLength() -> Bool {
+        return password.count >= 8 && password.count <= 16
+    }
+    
+    func doPasswordsMatch() -> Bool {
+        return password == repeatPassword
+    }
+    
+    func isValidPassword() -> Bool {
+        return isValidPasswordLength() && doPasswordsMatch()
+    }
+    
+    func isDataValid() -> Bool {
+        return isValidFirstName() && isValidLastName() && isValidEmail() && isValidPasswordLength() &&
+        doPasswordsMatch()
+    }
+}
---
+++ TestCaseStudyOfRegistrationFlowTests/RegistrationModelTestCase.swift
@@ -0,0 +1,140 @@
+//
+//  RegistrationModelTestCase.swift
+//  TestCaseStudyOfRegistrationFlowTests
+//
+//  Created by Ajit Satarkar on 04/01/24.
+//
+
+import XCTest
+
+import XCTest
+@testable import TestCaseStudyOfRegistrationFlow
+
+class RegistrationModelTestCase: XCTestCase {
+    
+    var sut: UserRegistrationModelValidatorProtocol!
+    let firstName = "Ajit"
+    let lastName = "Satarkar"
+    let email = "ajit.str@gmail.com"
+    let password = "12345678"
+    let repeatPassword = "12345678"
+    
+    override func setUp() {
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+    
+    func testUserModelStruc_canCreateNewInstance() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertNotNil(sut)
+     }
+ 
+    func testUserFirstName_shouldPassIfValidFirstName() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertTrue(sut.isValidFirstName())
+     }
+    
+    func testUserFirstName_shouldPassIfFirstNameLessThanMinLength() {
+        sut = UserRegistrationModel(firstName: "S",
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertFalse(sut.isValidFirstName())
+    }
+    
+    func testUserLastName_shouldPassIfValidLastName() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertTrue(sut.isValidLastName())
+    }
+    
+    func testUserLastName_shouldPassIfLastNameLessThanMinLength() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: "K",
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertFalse(sut.isValidLastName())
+    }
+    
+    func testUserRegistrationModel_shouldPassIfValidEmail() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertTrue(sut.isValidEmail())
+    }
+    
+    func testUserRegistrationModel_shouldPassIfInValidEmail() {
+        
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: "test.com",
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertFalse(sut.isValidEmail())
+    }
+    
+    func testUserRegistrationModel_shouldPassIfValidPasswordLength() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertTrue(sut.isValidPasswordLength())
+    }
+    
+    func testUserPassword_passwordAndRepeatPasswordMustMatch() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertTrue(sut.doPasswordsMatch())
+    }
+    
+    func testUserPassword_shouldPassIfPasswordIsValid() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        
+        XCTAssertTrue(sut.isValidPassword())
+    }
+    
+    func testUserData_shouldPassIfDataIsValid() {
+        sut = UserRegistrationModel(firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        repeatPassword: repeatPassword)
+        XCTAssertTrue(sut.isDataValid())
+    }
+
+}
